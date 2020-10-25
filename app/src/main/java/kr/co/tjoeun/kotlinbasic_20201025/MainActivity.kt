db87d8fc77110b1b3a42a52024fd65201f68bcef
조경진 <kj_cho@idealidea.co.kr>
2020-10-25 11:32:50 +0900

    입력된 문구를 -> 변수에 담아서 -> 텍스트뷰에 반영

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
@@ -34,5 +34,20 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        okBtn.setOnClickListener {
+
+//            Ctrl + / : 해당 줄을 주석으로 변경
+
+//            okBtn이 눌리면
+//            1) 뭐라고 입력했는지 조회 (contentEdt -> text를 get)
+//            조회한 내용을 기록해둬야, 2번에서 사용한다.
+
+            val inputContent = contentEdt.text
+
+//            2) 그 내용을 텍스트뷰에 반영 (contentTxt -> text를 set)
+            contentTxt.text = inputContent
+
+        }
+
     }
 }
