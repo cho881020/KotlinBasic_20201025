602bc3b67d9315f14f0a185cb39309f657e36d86
조경진 <kj_cho@idealidea.co.kr>
2020-10-25 10:26:48 +0900

    로그버튼 누르면 => 로그캣에 로그 찍기

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20201025/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.co.tjoeun.kotlinbasic_20201025
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logBtn.setOnClickListener {
+
+            Log.d("메인화면", "로그 버튼 눌림")
+
+        }
+
     }
 }
